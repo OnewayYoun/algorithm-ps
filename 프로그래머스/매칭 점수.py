75fd2e2885d794eaa1a549bfb4ec086224f3260e
OnewayYoun <onewayy91@gmail.com>
2021-06-26 02:32:28 +0900

    매칭 점수(문자열 처리) - 틀린 테케들 수정 완료

--- 프로그래머스/매칭 점수.py
+++ 프로그래머스/매칭 점수.py
@@ -1,6 +1,8 @@
 pages = ["<html lang=\"ko\" xml:lang=\"ko\" xmlns=\"http://www.w3.org/1999/xhtml\">\n<head>\n  <meta charset=\"utf-8\">\n  <meta property=\"og:url\" content=\"https://a.com\"/>\n</head>  \n<body>\nBlind Lorem Blind ipsum dolor Blind test sit amet, consectetur adipiscing elit. \n<a href=\"https://b.com\"> Link to b </a>\n</body>\n</html>", "<html lang=\"ko\" xml:lang=\"ko\" xmlns=\"http://www.w3.org/1999/xhtml\">\n<head>\n  <meta charset=\"utf-8\">\n  <meta property=\"og:url\" content=\"https://b.com\"/>\n</head>  \n<body>\nSuspendisse potenti. Vivamus venenatis tellus non turpis bibendum, \n<a href=\"https://a.com\"> Link to a </a>\nblind sed congue urna varius. Suspendisse feugiat nisl ligula, quis malesuada felis hendrerit ut.\n<a href=\"https://c.com\"> Link to c </a>\n</body>\n</html>", "<html lang=\"ko\" xml:lang=\"ko\" xmlns=\"http://www.w3.org/1999/xhtml\">\n<head>\n  <meta charset=\"utf-8\">\n  <meta property=\"og:url\" content=\"https://c.com\"/>\n</head>  \n<body>\nUt condimentum urna at felis sodales rutrum. Sed dapibus cursus diam, non interdum nulla tempor nec. Phasellus rutrum enim at orci consectetu blind\n<a href=\"https://a.com\"> Link to a </a>\n</body>\n</html>"]
+#pages = ["<html lang=\"ko\" xml:lang=\"ko\" xmlns=\"http://www.w3.org/1999/xhtml\">\n<head>\n  <meta charset=\"utf-8\">\n  <meta property=\"og:url\" content=\"http://a.com\"/>\n</head>  \n<body>\nBlind Lorem Blind ipsum dolor Blind test sit amet, consectetur adipiscing elit. \n<a href=\"http://b.com\"> Link to b </a>\n</body>\n</html>", "<html lang=\"ko\" xml:lang=\"ko\" xmlns=\"http://www.w3.org/1999/xhtml\">\n<head>\n  <meta charset=\"utf-8\">\n  <meta property=\"og:url\" content=\"https://b.com\"/>\n</head>  \n<body>\nSuspendisse potenti. Vivamus venenatis tellus non turpis bibendum, \n<a href=\"https://a.com\"> Link to a </a>\nblind sed congue urna varius. Suspendisse feugiat nisl ligula, quis malesuada felis hendrerit ut.\n<a href=\"https://c.com\"> Link to c </a>\n</body>\n</html>", "<html lang=\"ko\" xml:lang=\"ko\" xmlns=\"http://www.w3.org/1999/xhtml\">\n<head>\n  <meta charset=\"utf-8\">\n  <meta property=\"og:url\" content=\"https://c.com\"/>\n</head>  \n<body>\nUt condimentum urna at felis sodales rutrum. Sed dapibus cursus diam, non interdum nulla tempor nec. Phasellus rutrum enim at orci consectetu blind\n<a href=\"https://a.com\"> Link to a </a>\n</body>\n</html>"]
 word = 'blind'
 
+#0muzi0muzi0
 '''
 a = <html lang="ko" xml:lang="ko" xmlns="http://www.w3.org/1999/xhtml">
 <head>
@@ -44,17 +46,22 @@ Ut condimentum urna at felis sodales rutrum. Sed dapibus cursus diam, non interd
 '''
 import re
 
-
-
 def solution(word, pages):
     scores = []
 
-    for i, v in enumerate(pages):
-        basic = len(re.findall('[^a-z]' + word.lower() + '[^a-z]', v.lower()))
-        ext_link = v.lower().count('</a>')
-        link = float(basic/ext_link)
-        content = re.findall('(?<=content=").+?(?="/>)', v)[0]
-        ext_link_list = re.findall('(?<=<a href=").+?(?=">)', v)
+    for i in pages:
+        #전방 후방탐색 이용해서
+        #test = '0muzi0muzi0', word = 'muzi' 이런 경우 처리
+        basic = len(re.findall('(?<=[^a-z])' + word.lower() + '(?=[^a-z])', i.lower()))
+        ext_link = i.lower().count('</a>')
+        # basic / ext_link 에서 ext_link 가 0일 경우 예외처리해서 오류 방지
+        if ext_link != 0:
+            link = float(basic / ext_link)
+        else:
+            link = 0
+        content = re.findall('(?<=<meta property="og:url" content=")https.+?(?="/ ?>)', i)
+        content = [] if not content else content[0]
+        ext_link_list = re.findall('(?<=<a href=")https.+?(?=">)', i)
         scores.append((basic, link, content, ext_link_list))
 
     answer = []
@@ -66,10 +73,9 @@ def solution(word, pages):
                 score += scores[j][1]
         answer.append(score)
 
+    print(scores)
+    print(answer)
 
     return answer.index(max(answer))
-
-
-
 
 print(solution(word, pages))
