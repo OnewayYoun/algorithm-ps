5a05641d6d9d66e4d920846692bff06287e02382
OnewayYoun <onewayy91@gmail.com>
2021-06-27 04:00:50 +0900

    땅따먹기(lv2) - 로직

---
+++ 프로그래머스/땅따먹기.py
@@ -0,0 +1,14 @@
+land = [[1,2,3,5],[5,6,7,8],[4,3,2,1]]
+#answer = 16
+
+def solution(land):
+
+    for i in range(0, len(land)-1):
+        for j in range(4):
+            land[i+1][j] = max(land[i][:j] + land[i][j+1:]) + land[i+1][j]
+
+    return max(land[len(land)-1])
+
+print(solution([[1,2,3,5],[5,6,7,8],[4,3,2,1]]))
+
+#출처: 프로그래머스 코딩테스트 연습, https://programmers.co.kr/learn/courses/30/lessons/12913
