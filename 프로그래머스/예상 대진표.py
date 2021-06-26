2c0e89e4a0bc900a5cf4dd254ff12c9d2675d6ba
OnewayYoun <onewayy91@gmail.com>
2021-06-26 23:52:43 +0900

    예살 대진표(lv2)

---
+++ 프로그래머스/예상 대진표.py
@@ -0,0 +1,13 @@
+def solution(n,a,b):
+    answer = 0
+
+    while a != b:
+        answer += 1
+        a = (a+1) // 2
+        b = (b+1) // 2
+
+    return answer
+
+print(solution(8, 4, 7))
+
+#출처: 프로그래머스 코딩테스트 연습, https://programmers.co.kr/learn/courses/30/lessons/12985
