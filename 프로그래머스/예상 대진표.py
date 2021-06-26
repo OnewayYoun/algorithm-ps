d3c22567ca3b6ff3b0c2de94540ed66e3d8665ba
OnewayYoun <onewayy91@gmail.com>
2021-06-26 23:54:11 +0900

    예살 대진표(lv2) - 비트풀이 추가

--- 프로그래머스/예상 대진표.py
+++ 프로그래머스/예상 대진표.py
@@ -11,3 +11,10 @@ def solution(n,a,b):
 print(solution(8, 4, 7))
 
 #출처: 프로그래머스 코딩테스트 연습, https://programmers.co.kr/learn/courses/30/lessons/12985
+
+
+'''
+비트로 푸는 방식
+def solution(n,a,b):
+    return ((a-1)^(b-1)).bit_length()
+'''
