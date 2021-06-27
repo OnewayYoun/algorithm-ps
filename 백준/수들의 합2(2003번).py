7a710166b6d1926919396cb0e924304566d4a92c
OnewayYoun <onewayy91@gmail.com>
2021-06-27 09:54:11 +0900

    수들의 합(투포인터)

---
+++ 백준/수들의 합2(2003번).py
@@ -0,0 +1,26 @@
+'''
+4 2
+1 1 1 1
+answer = 3
+10 5
+1 2 3 4 2 5 3 1 1 2
+answer = 3
+'''
+
+n, m = map(int, input().split())
+pers = list(map(int, input().split()))
+end = 0
+sum = 0
+answer = 0
+
+
+for start in range(n):
+    while sum < m and end < n:
+        sum += pers[end]
+        end += 1
+    if sum == m:
+        answer += 1
+    sum -= pers[start]
+
+print(answer)
+print(sum)
