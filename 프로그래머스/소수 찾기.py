75ffa11c7beeaea01fc6bd720dd16e08da5b9878
OnewayYoun <onewayy91@gmail.com>
2021-07-05 18:06:11 +0900

    소수 찾기(완탐)

---
+++ 프로그래머스/소수 찾기.py
@@ -0,0 +1,32 @@
+'''
+numbers	return
+"17"	3
+"011"	2
+'''
+from itertools import permutations
+
+def is_prime(x):
+    x = int(x)
+    if x < 2:
+        return False
+    for i in range(2, x):
+        if x%i == 0:
+            return False
+    return True
+
+def solution(numbers):
+    answer = set()
+    for i in range(1, len(numbers)+1):
+        tmp = list(map(''.join, permutations(numbers, i)))
+        for j in tmp:
+            if is_prime(j):
+                answer.add(int(j))
+    print(answer)
+    return len(answer)
+
+print(list(map(''.join, permutations('011', 1))))
+print(list(map(''.join, permutations('011', 2))))
+print(list(map(''.join, permutations('011', 3))))
+
+
+print(solution("011"))
