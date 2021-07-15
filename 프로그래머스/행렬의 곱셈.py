e4c7c573d4861c57c9430431b8cfa71b3e801567
OnewayYoun <onewayy91@gmail.com>
2021-07-15 19:23:43 +0900

    행렬의 곱셈(추가)

--- 프로그래머스/행렬의 곱셈.py
+++ 프로그래머스/행렬의 곱셈.py
@@ -3,6 +3,8 @@ arr1	                            arr2	                            return
 [[1, 4], [3, 2], [4, 1]]	        [[3, 3], [3, 3]]	                [[15, 15], [15, 15], [15, 15]]
 [[2, 3, 2], [4, 2, 4], [3, 1, 4]]	[[5, 4, 3], [2, 4, 1], [3, 1, 1]]	[[22, 22, 11], [36, 28, 18], [29, 20, 14]]
 '''
+a = [[2, 3, 2], [4, 2, 4], [3, 1, 4]]
+b = [[5, 4, 3], [2, 4, 1], [3, 1, 1]]
 
 def solution(arr1, arr2):
     answer = []
@@ -17,7 +19,10 @@ def solution(arr1, arr2):
     return answer
 
 
-a = [[1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1]]
-b = [[1, 1, 1, 1, 1, 1, 1], [1, 1, 1, 1, 1, 1, 1]]
-print(solution(a, b))
+def solution1(arr1, arr2):
+    return [[sum(a*b for a, b in zip(A_row, B_col)) for B_col in zip(*arr2)] for A_row in arr1]
+
+print(solution1(a, b))
+
+
 
