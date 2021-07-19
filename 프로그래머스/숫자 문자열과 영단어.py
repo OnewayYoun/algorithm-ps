9bd07db9852d411b5316a00ff080244c25e60227
OnewayYoun <onewayy91@gmail.com>
2021-07-19 23:10:55 +0900

    숫자 문자열과 영단어

---
+++ 프로그래머스/숫자 문자열과 영단어.py
@@ -0,0 +1,25 @@
+'''
+s	                result
+"one4seveneight"	1478
+"23four5six7"	    234567
+"2three45sixseven"	234567
+"123"	            123
+
+0	zero
+1	one
+2	two
+3	three
+4	four
+5	five
+6	six
+7	seven
+8	eight
+9	nine
+'''
+
+#.03ms
+def solution(s):
+    return int(s.lower().replace('zero', '0').replace('one', '1').replace('two', '2').replace('three', '3').replace('four', '4').replace('five', '5').replace('six', '6').replace('seven', '7').replace('eight', '8').replace('nine', '9'))
+
+
+print(solution("2three45Sixseven"))
