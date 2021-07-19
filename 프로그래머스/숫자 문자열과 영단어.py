42fa73e3129dbdc139e18f61ccb38cc115dab053
OnewayYoun <onewayy91@gmail.com>
2021-07-19 23:22:02 +0900

    숫자 문자열과 영단어(using dict)-added

--- 프로그래머스/숫자 문자열과 영단어.py
+++ 프로그래머스/숫자 문자열과 영단어.py
@@ -17,9 +17,32 @@ s	                result
 9	nine
 '''
 
-#.03ms
+# 0.03ms
 def solution(s):
     return int(s.lower().replace('zero', '0').replace('one', '1').replace('two', '2').replace('three', '3').replace('four', '4').replace('five', '5').replace('six', '6').replace('seven', '7').replace('eight', '8').replace('nine', '9'))
 
 
+# 0.03ms
+num_dict = {
+    'zero': '0',
+    'one': '1',
+    'two': '2',
+    'three': '3',
+    'four': '4',
+    'five': '5',
+    'six': '6',
+    'seven': '7',
+    'eight': '8',
+    'nine': '9'
+}
+
+
+def solution1(s):
+    s = s.lower()
+    for k, v in num_dict.items():
+        s = s.replace(k, v)
+    return int(s)
+
+
 print(solution("2three45Sixseven"))
+print(solution1("2three45Sixseven"))
