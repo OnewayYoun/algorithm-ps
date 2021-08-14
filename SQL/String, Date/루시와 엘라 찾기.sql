5e367f933bef11826345b4e1b12724626e2c88f9
OnewayYoun <onewayy91@gmail.com>
2021-08-14 11:57:27 +0900

    SQL practice

---
+++ SQL/String, Date/루시와 엘라 찾기.sql
@@ -0,0 +1,8 @@
+/*
+동물 보호소에 들어온 동물 중 이름이 Lucy, Ella, Pickle, Rogan, Sabrina, Mitty인 동물의 아이디와 이름, 성별 및 중성화 여부를 조회하는 SQL 문을 작성해주세요.
+*/
+
+-- 코드를 입력하세요
+SELECT animal_id, name, sex_upon_intake
+FROM animal_ins
+WHERE name IN ('Lucy', 'Ella', 'Pickle', 'Rogan', 'Sabrina', 'Mitty')
