9aa1c998562c3f55bb9b524601ed0b0a7a5062ac
OnewayYoun <onewayy91@gmail.com>
2021-10-30 20:53:19 +0900

    create 위클리 - 5주차.py

---
+++ 프로그래머스/위클리 - 5주차.py
@@ -0,0 +1,21 @@
+def solution(word):
+    answer = 0
+    return answer
+
+
+if __name__ == '__main__':
+    word = "AAAAE"
+    result = 6
+    print(solution(word))
+
+    word = "AAAE"
+    result = 10
+    print(solution(word))
+
+    word = "I"
+    result = 1563
+    print(solution(word))
+
+    word = "EIO"
+    result = 1189
+    print(solution(word))
