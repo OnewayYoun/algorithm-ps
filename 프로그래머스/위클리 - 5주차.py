25ba22f1d685bfdebd1b0f1d608523079b77b1c2
OnewayYoun <onewayy91@gmail.com>
2021-10-31 03:18:10 +0900

    update 위클리 - 5주차.py(solved with product)

--- 프로그래머스/위클리 - 5주차.py
+++ 프로그래머스/위클리 - 5주차.py
@@ -1,6 +1,16 @@
+from itertools import product
+
 def solution(word):
-    answer = 0
-    return answer
+    answer = []
+    words = ['A', 'E', 'I', 'O', 'U']
+
+    for i in range(6):
+        for j in product(words, repeat=i):
+            answer.append(''.join(j))
+
+    answer.sort()
+
+    return answer.index(word)
 
 
 if __name__ == '__main__':
