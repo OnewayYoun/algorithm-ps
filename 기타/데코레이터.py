1ea2e99a041fdf5664882a38e7bd550bafedca7c
OnewayYoun <onewayy91@gmail.com>
2021-11-16 16:29:30 +0900

    데코레이터 연습

--- 기타/데코레이터.py
+++ 기타/데코레이터.py
@@ -18,19 +18,80 @@
 # x()
 
 
-def f1(func):
-    def wrapper():
-        print('started')
-        func()
-        print('ended')
+# def f1(func):
+#     def wrapper():
+#         print('started')
+#         func()
+#         print('ended')
+#
+#     return wrapper
+#
+#
+# @f1  # 데코레이터 붙이면 f2 = f1(f2)
+# def f2():
+#     print('hello')
+#
+#
+# print(f2)
+# f2()
+
+
+# def f1(func):
+#     def wrapper(*args, **kwargs):
+#         print('started')
+#         func(*args, **kwargs)
+#         print('ended')
+#
+#     return wrapper
+#
+#
+# @f1  # 데코레이터 붙이면 f2 = f1(f2)
+# def f2(a, b=9):
+#     print(a, b)
+#
+#
+# f2('hi')
+
+
+# def f1(func):
+#     def wrapper(*args, **kwargs):
+#         print('started')
+#         val = func(*args, **kwargs)
+#         print('ended')
+#         return val
+#
+#     return wrapper
+#
+#
+# @f1  # 데코레이터 붙이면 f2 = f1(f2)
+# def f2(a, b=9):
+#     print(a, b)
+#
+#
+# @f1  # 데코레이터 붙이면 f2 = f1(f2)
+# def add(x, y):
+#     return x + y
+#
+#
+# f2('hi')
+# print()
+# print(add(1, 5))
+
+
+def before_after(func):
+    def wrapper(*args):
+        print('Before')
+        func(*args)
+        print('After')
 
     return wrapper
 
 
-@f1  # 데코레이터 붙이면 f2 = f1(f2)
-def f2():
-    print('hello')
+class Test:
+    @before_after
+    def decorated_method(self):
+        print('run')
 
 
-print(f2)
-f2()
+t = Test()
+t.decorated_method()
