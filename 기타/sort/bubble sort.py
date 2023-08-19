e0063816c3798b2dbc28263a9161ef10f731887c
OnewayYoun <onewayy91@gmail.com>
2023-08-20 02:29:56 +0900

    bubble sort

---
+++ 기타/sort/bubble sort.py
@@ -0,0 +1,16 @@
+def bubble_sort(lst: list) -> list:
+    for i in range(len(lst) - 1):
+        flag = True
+        for j in range(len(lst) - 1 - i):
+            if lst[j] > lst[j + 1]:
+                flag = False
+                lst[j], lst[j + 1] = lst[j + 1], lst[j]
+        if flag:
+            break
+
+
+if __name__ == '__main__':
+    tmp = [3, 1, 2, 4, 2, 5, 1, 3]
+    # tmp = [1, 2, 3, 4, 5]
+    bubble_sort(tmp)
+    print(tmp)
