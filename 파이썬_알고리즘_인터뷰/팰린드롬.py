77d49512e95e88e869bd1c22bd90d575aef561ae
hankil_youn <hankil_youn@tmax.co.kr>
2024-10-07 17:41:48 +0900

    feat: add isPalindrome function

---
+++ 파이썬_알고리즘_인터뷰/팰린드롬.py
@@ -0,0 +1,30 @@
+from collections import deque
+import re
+
+
+def solution_with_list(string: str) -> bool:
+    strs = [char for char in string if char.isalnum()]
+    while len(strs) > 1:
+        if strs.pop(0) != strs.pop(-1):
+            return False
+    return True
+
+
+def solution_with_deque(string: str) -> bool:
+    dq = deque([char for char in string if char.isalnum()])
+    while len(dq) > 1:
+        if dq.popleft() != dq.pop():
+            return False
+    return True
+
+
+def solution_with_slicing(string: str) -> bool:
+    string = string.lower()
+    string = re.sub('[^a-z0-9]', '', string)
+    return string == string[::-1]
+
+
+if __name__ == '__main__':
+    print(solution_with_list("a::a"))
+    print(solution_with_deque("a::a"))
+    print(solution_with_slicing("a::a"))
