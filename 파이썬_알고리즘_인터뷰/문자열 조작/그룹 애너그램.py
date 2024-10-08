1b514692e5b22233e3ece476a9faf8cf0c34d8d8
hankil_youn <onewayy91@gmail.com>
2024-10-08 18:21:28 +0900

    feat: 그룹 애너그램

---
+++ 파이썬_알고리즘_인터뷰/문자열 조작/그룹 애너그램.py
@@ -0,0 +1,9 @@
+import collections
+
+words = ['eat', 'tea', 'tan', 'ate', 'nat', 'bat']
+anagrams = collections.defaultdict(list)
+
+for word in words:
+    anagrams[''.join(sorted(word))].append(word)
+
+print(anagrams.values())
