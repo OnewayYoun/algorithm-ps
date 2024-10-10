deccf9f2a38c9b4c9bd054627ebb4f4d284828b7
hankil_youn <onewayy91@gmail.com>
2024-10-11 01:11:52 +0900

    feat: 팩토리얼 연습

---
+++ 이코테/팩토리얼.py
@@ -0,0 +1,27 @@
+def factorial_iterative(n):
+    res = 1
+    for i in range(n, 0, -1):
+        res *= i
+    return res
+
+
+def factorial_recursive(n):
+    if n == 1:
+        return 1
+    res = n * factorial_recursive(n - 1)
+    return res
+
+
+print(factorial_recursive(5))
+
+# def recursive(i):
+#     if i == 3:
+#         return 'hi'
+#     print(f'{i} 번째 재귀함수에서, {i+1}번째 재귀 함수를 호출함')
+#     res = recursive(i + 1)
+#     print(f'{i} 번째 재귀함수를 종료함')
+#     print(res)
+#     return i
+#
+#
+# print(recursive(1))
