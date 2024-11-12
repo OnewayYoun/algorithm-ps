1fe63983d8455306a7d1d7b7a2389b021ea8ac07
hankil_youn <onewayy91@gmail.com>
2024-11-12 17:44:13 +0900

    feat: unique paths(dp)

--- LeetCode/DP/Unique Paths.py
+++ LeetCode/DP/Unique Paths.py
@@ -22,5 +22,20 @@ class Solution:
         return dp[key]
 
 
+class Solution2:
+    def uniquePaths(self, m: int, n: int, dp=None) -> int:
+        if dp is None:
+            dp = {}
+        if (m, n) in dp:
+            return dp[(m, n)]
+        if m == 1 or n == 1:
+            return 1
+        if m == 0 or n == 0:
+            return 0
+        dp[(m, n)] = self.uniquePaths(m - 1, n, dp) + self.uniquePaths(m, n - 1, dp)
+        return dp[(m, n)]
+
+
 if __name__ == '__main__':
     print(Solution().uniquePaths(2, 3))
+    print(Solution2().uniquePaths(2, 3))
