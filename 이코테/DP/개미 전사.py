7c6b94bf346ba406682f120ed0f88dcce4b3b4c5
hankil_youn <onewayy91@gmail.com>
2024-11-18 22:05:49 +0900

    feat: recursive solution

--- 이코테/DP/개미 전사.py
+++ 이코테/DP/개미 전사.py
@@ -1,3 +1,8 @@
+"""
+4
+1 3 1 5
+"""
+
 n = int(input())
 l = list(map(int, input().split()))
 
@@ -6,6 +11,20 @@ dp[0] = l[0]
 dp[1] = max(l[0], l[1])
 
 for i in range(2, n):
-    dp[i] = max(dp[i-1], dp[i-2] + l[i])
+    dp[i] = max(dp[i - 1], dp[i - 2] + l[i])
 
-print(dp[n-1])
+print(dp[n - 1])
+
+dp = {}
+
+
+def recursive(i):
+    if i == 0:
+        return l[0]
+    if i == 1:
+        return max(l[0], l[1])
+    dp[i] = max(recursive(i - 1), recursive(i - 2) + l[i])
+    return dp[i]
+
+
+print(recursive(len(l) - 1))
