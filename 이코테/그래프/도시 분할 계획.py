3b6bde49e2200921209c89b2303116e6a41dfe3e
hankil_youn <onewayy91@gmail.com>
2024-11-24 18:01:10 +0900

    feat: 도시 분할 계획

---
+++ 이코테/그래프/도시 분할 계획.py
@@ -0,0 +1,55 @@
+def find_parent(parent, x):
+    if parent[x] != x:
+        parent[x] = find_parent(parent, parent[x])
+    return parent[x]
+
+
+def union_parent(parent, a, b):
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+
+n, m = map(int, input().split())
+parent = [0] * (n + 1)
+for i in range(1, n + 1):
+    parent[i] = i
+
+edges = []
+
+for _ in range(m):
+    a, b, c = map(int, input().split())
+    edges.append((c, a, b))
+
+edges.sort()
+result = 0
+max_val = float("-inf")
+
+for edge in edges:
+    cost, a, b = edge
+    if find_parent(parent, a) != find_parent(parent, b):
+        union_parent(parent, a, b)
+        result += cost
+        max_val = cost
+
+print(result - max_val)
+
+
+"""
+7 12
+1 2 3
+1 3 2
+3 2 1
+2 5 2
+3 4 4
+7 3 6
+5 1 5
+1 6 2
+6 4 1
+6 5 3
+4 5 3
+6 7 4
+"""
