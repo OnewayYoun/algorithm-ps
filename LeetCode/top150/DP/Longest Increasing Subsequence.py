c081c8200b3980463e38cf9d7c0529b48f75859a
hankil_youn <onewayy91@gmail.com>
2024-11-26 00:32:56 +0900

    feat: solve with binary search

--- LeetCode/top150/DP/Longest Increasing Subsequence.py
+++ LeetCode/top150/DP/Longest Increasing Subsequence.py
@@ -1,4 +1,5 @@
 from typing import List
+from bisect import bisect_left
 
 
 class Solution:
@@ -14,4 +15,16 @@ class Solution:
         return max(dp)
 
 
+class Solution2:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        answer = [nums[0]]
+        for val in nums[1:]:
+            if val > answer[-1]:
+                answer.append(val)
+            else:
+                answer[bisect_left(answer, val)] = val
+        return len(answer)
+
+
 print(Solution().lengthOfLIS([10, 9, 2, 5, 3, 7, 101, 18]))
+print(Solution2().lengthOfLIS([10, 9, 2, 5, 3, 7, 101, 18]))
