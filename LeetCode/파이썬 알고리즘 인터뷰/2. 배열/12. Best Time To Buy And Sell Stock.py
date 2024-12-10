e58f1bec09eae41667f83ae6de015d17ad6a9b18
hankil_youn <onewayy91@gmail.com>
2024-12-10 20:05:27 +0900

    feat: Best Time to Buy and Sell Stock II

--- LeetCode/파이썬 알고리즘 인터뷰/2. 배열/12. Best Time To Buy And Sell Stock.py
+++ LeetCode/파이썬 알고리즘 인터뷰/2. 배열/12. Best Time To Buy And Sell Stock.py
@@ -20,6 +20,28 @@ class Solution:
 
         return max_profit
 
+    """
+    Best Time to Buy and Sell Stock II
+    
+    Input: prices = [7,1,5,3,6,4]
+    Output: 7
+    
+    Input: prices = [1,2,3,4,5]
+    Output: 4
+    
+    Input: prices = [7,6,4,3,1]
+    Output: 0
+    """
+
+    def maxProfit2(self, prices: List[int]) -> int:
+        profit = 0
+        for i in range(len(prices)):
+            if i == 0:
+                continue
+            if prices[i - 1] < prices[i]:
+                profit += prices[i] - prices[i - 1]
+        return profit
+
 
 print(Solution().maxProfit([7, 1, 5, 3, 6, 4]))
 print(Solution().maxProfit([7, 6, 4, 3, 1]))
