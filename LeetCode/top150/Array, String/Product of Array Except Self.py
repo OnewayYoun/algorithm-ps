7dccde8fd1a505bfdbc493bb861b689dbc019a1e
hankil_youn <onewayy91@gmail.com>
2025-01-06 15:30:36 +0900

    feat: solve with prefix_product

--- LeetCode/top150/Array, String/Product of Array Except Self.py
+++ LeetCode/top150/Array, String/Product of Array Except Self.py
@@ -17,12 +17,26 @@ class Solution:
         for num in nums:
             if num == 0:
                 cnt_zero += 1
-
         if cnt_zero > 1:
             return [0 for _ in range(len(nums))]
         elif cnt_zero == 1:
             return [total if num == 0 else 0 for num in nums]
         return [total // num for num in nums]
 
+    def productExceptSelf2(self, nums: List[int]) -> List[int]:
+        left_sum, right_sum = 1, 1
+        product_sum_right = [1]
+        product_sum_left = [1]
 
-print(Solution().productExceptSelf([-1, 1, 0, -3, 3]))
+        for i in range(len(nums)):
+            right_sum *= nums[i]
+            left_sum *= nums[-i - 1]
+            product_sum_right.append(right_sum)
+            product_sum_left.append(left_sum)
+        product_sum_left = product_sum_left[::-1]
+
+        return [product_sum_right[i] * product_sum_left[i + 1] for i in range(len(nums))]
+
+
+# print(Solution().productExceptSelf([-1, 1, 0, -3, 3]))
+print(Solution().productExceptSelf2([2, 2, 3, 4]))
