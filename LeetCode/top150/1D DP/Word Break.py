252ecc7016748ff9ac94453996df192055ff6936
hankil_youn <onewayy91@gmail.com>
2025-02-03 14:15:34 +0900

    feat: solve with DP(bottom-up)

---
+++ LeetCode/top150/1D DP/Word Break.py
@@ -0,0 +1,29 @@
+from typing import List
+
+
+class Solution:
+    """
+    Input: s = "leetcode", wordDict = ["leet","code"]
+    Output: true
+
+    Input: s = "applepenapple", wordDict = ["apple","pen"]
+    Output: true
+
+    Input: s = "catsandog", wordDict = ["cats","dog","sand","and","cat"]
+    Output: false
+    """
+
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        dp = [False] * (len(s) + 1)
+        dp[0] = True
+
+        for i in range(len(s) + 1):
+            for word in wordDict:
+                if s[i:i + len(word)] == word and dp[i]:
+                    if i + len(word) == len(s) + 1:
+                        return True
+                    dp[i + len(word)] = True
+        return dp[len(s)]
+
+
+Solution().wordBreak(s="cars", wordDict=["car", "ca", "rs"])
