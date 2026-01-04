5b79c435fe1c8243e8c14ca7ef42e6d9f2980c67
hankil_youn <onewayy91@gmail.com>
2026-01-04 19:31:57 +0900

    ps: solve again

--- LeetCode/파이썬 알고리즘 인터뷰/1. 문자열 조작/937. Reorder Data in Log Files.py
+++ LeetCode/파이썬 알고리즘 인터뷰/1. 문자열 조작/937. Reorder Data in Log Files.py
@@ -3,11 +3,15 @@ from typing import List
 
 class Solution:
     def reorderLogFiles(self, logs: List[str]) -> List[str]:
-        letter_list, digit_list = [], []
+        letter_logs, digit_logs = [], []
         for log in logs:
-            if log.split()[1].isdigit():
-                digit_list.append(log)
+            if log.split()[1].isalpha():
+                letter_logs.append(log)
             else:
-                letter_list.append(log)
-        letter_list.sort(key=lambda x: (' '.join(x.split()[1:]), x.split()[0]))
-        return letter_list + digit_list
+                digit_logs.append(log)
+
+        return sorted(letter_logs, key=lambda x: (x.split(" ", 1)[1], x)) + digit_logs
+
+
+if __name__ == "__main__":
+    print(Solution().reorderLogFiles(["dig1 8 1 5 1", "let2 art can", "dig2 3 6", "let1 art can", "let3 art zero"]))
