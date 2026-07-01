8737e8e95bcab40a83a95f0b23060f0b7bc96cc9
hankil_youn <onewayy91@gmail.com>
2026-07-01 19:28:41 +0900

    ps: Remove Duplicates from Sorted Array II(leetcode 80)

--- LeetCode/top150/Array, String/Remove Duplicates from Sorted Array II(re).py
+++ LeetCode/top150/Array, String/Remove Duplicates from Sorted Array II(re).py
@@ -18,5 +18,19 @@ class Solution:
                 i += 1
         return i
 
+    def removeDuplicates1(self, nums: List[int]) -> int:
+        idx = 0
+        cnt = float('inf')
+        cur_num = float('-inf')
+        for num in nums:
+            if num != cur_num:
+                cur_num = num
+                cnt = 0
+            if cnt < 2:
+                nums[idx] = cur_num
+                idx += 1
+            cnt += 1
+        return idx
 
-print(Solution().removeDuplicates([0, 0, 1, 1, 1, 1, 2, 3, 3]))
+# print(Solution().removeDuplicates([0, 0, 1, 1, 1, 1, 2, 3, 3]))
+print(Solution().removeDuplicates1([0, 0, 1, 1, 1, 1, 2, 3, 3]))
