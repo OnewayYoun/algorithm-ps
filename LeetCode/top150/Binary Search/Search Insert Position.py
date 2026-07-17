db98f9fb6daabe00d96af4695272b64f1d707903
hankil_youn <onewayy91@gmail.com>
2026-07-17 13:52:37 +0900

    ps: Search Insert Position(leetcode 35)

--- LeetCode/top150/Binary Search/Search Insert Position.py
+++ LeetCode/top150/Binary Search/Search Insert Position.py
@@ -30,5 +30,38 @@ class Solution:
         return left
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    def searchInsert2(self, nums: List[int], target: int) -> int:
+        left, right = 0, len(nums) - 1
+
+        while left <= right:
+            mid = (left + right) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] > target:
+                right = mid - 1
+            elif nums[mid] < target:
+                left = mid + 1
+
+        return left
+
+
 # print(Solution().searchInsert(nums=[1, 3, 5, 6], target=5))
-print(Solution().searchInsert1(nums=[1, 3, 5, 6], target=0))
+print(Solution().searchInsert2(nums=[1, 3, 5, 6], target=2))
