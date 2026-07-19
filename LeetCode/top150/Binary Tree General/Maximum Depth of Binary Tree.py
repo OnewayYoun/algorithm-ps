8812a73104194a39a18fc717c700159b30948e27
hankil_youn <onewayy91@gmail.com>
2026-07-19 20:37:43 +0900

    ps: Maximum Depth of Binary Tree(leetcode 104)

--- LeetCode/top150/Binary Tree General/Maximum Depth of Binary Tree.py
+++ LeetCode/top150/Binary Tree General/Maximum Depth of Binary Tree.py
@@ -36,3 +36,75 @@ class Solution:
                 dq.append([node.right, level + 1])
 
         return max_depth
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    def maxDepth_dfs(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+        return 1 + max(self.maxDepth_dfs(root.left), self.maxDepth_dfs(root.right))
+
+    def maxDepth_dfs_iterative(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+        level = 1
+        stack = [[root, level]]
+
+        while stack:
+            node, cur_level = stack.pop()
+            if node:
+                level = max(level, cur_level)
+                stack.append([node.left, cur_level + 1])
+                stack.append([node.right, cur_level + 1])
+        return level
+
+    def maxDepth_bfs(self, root: Optional[TreeNode]) -> int:
+        level = 0
+        if not root:
+            return level
+
+        dq = deque([root])
+        while dq:
+            for _ in range(len(dq)):
+                node = dq.popleft()
+                if node.left:
+                    dq.append(node.left)
+                if node.right:
+                    dq.append(node.right)
+            level += 1
+        return level
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    root = TreeNode(3)
+    root.left = TreeNode(9)
+    root.right = TreeNode(20)
+    root.right.left = TreeNode(15)
+    root.right.right = TreeNode(7)
+
+    print(Solution().maxDepth_dfs_iterative(root))
