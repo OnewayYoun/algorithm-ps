102c0641af80691fce784d09cb890a970596ad20
hankil_youn <onewayy91@gmail.com>
2026-07-20 17:25:01 +0900

    ps: Word Break(leetcode 139)

--- LeetCode/top150/1D DP/Word Break.py
+++ LeetCode/top150/1D DP/Word Break.py
@@ -25,5 +25,40 @@ class Solution:
                     dp[i + len(word)] = True
         return dp[len(s)]
 
+    def wordBreak_bottom_up(self, s: str, wordDict: List[str]) -> bool:
+        wordDict = set(wordDict)
+        dp = [False] * (len(s) + 1)
+        dp[0] = True
 
-print(Solution().wordBreak(s="cars", wordDict=["car", "ca", "rs"]))
+        for i in range(1, len(s) + 1):
+            for j in range(i):
+                if dp[j] and s[j:i] in wordDict:
+                    dp[i] = True
+                    break
+
+        return dp[-1]
+
+    def wordBreak_top_down(self, s: str, wordDict: List[str]) -> bool:
+        wordSet = set(wordDict)
+        memo = {}
+
+        def dfs(start):
+            if start == len(s):
+                return True
+
+            if start in memo:
+                return memo[start]
+
+            for end in range(start + 1, len(s) + 1):
+                word = s[start:end]
+                if word in wordSet:
+                    if dfs(end):
+                        memo[start] = True
+                        return True
+            memo[start] = False
+            return False
+
+        return dfs(0)
+
+
+print(Solution().wordBreak_bottom_up(s = "leetcode", wordDict = ["lee", "leet", "tc", "ode", "code"]))
