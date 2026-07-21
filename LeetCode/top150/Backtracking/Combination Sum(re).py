18c8d9c1474aaf15d75f6e99bd5ff2d9a06a7032
hankil_youn <onewayy91@gmail.com>
2026-07-21 15:57:17 +0900

    ps: Combination Sum(leetcode 39)

--- LeetCode/top150/Backtracking/Combination Sum(re).py
+++ LeetCode/top150/Backtracking/Combination Sum(re).py
@@ -22,4 +22,86 @@ class Solution:
         return answer
 
 
-print(Solution().combinationSum(candidates=[2, 3, 6, 7], target=7))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    def combinationSum1(self, candidates: List[int], target: int) -> List[List[int]]:
+        answer = []
+        cur_lst = []
+        cur_sum = 0
+
+        def dfs(idx):
+            nonlocal cur_sum
+
+            if idx == len(candidates):
+                return
+
+            if cur_sum == target:
+                answer.append(cur_lst[:])
+                return
+
+            if cur_sum > target:
+                return
+
+
+            cur_lst.append(candidates[idx])
+            cur_sum += candidates[idx]
+
+            dfs(idx)
+
+            cur_lst.pop()
+            cur_sum -= candidates[idx]
+
+            dfs(idx + 1)
+
+
+        dfs(0)
+
+        return answer
+
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        answer = []
+        cur_lst = []
+
+        def dfs(idx, cur_sum):
+            if cur_sum == target:
+                answer.append(cur_lst[:])
+                return
+
+            if cur_sum > target:
+                return
+
+            for i in range(idx, len(candidates)):
+                cur_lst.append(candidates[i])
+                dfs(i, cur_sum + candidates[i])
+                cur_lst.pop()
+
+        dfs(0, 0)
+
+        return answer
+
+print(Solution().combinationSum2(candidates=[1, 3, 6, 7], target=7))
